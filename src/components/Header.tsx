e96fea9bbcbd1506a721c550c85d0115a58c15c5
Aleksandr <strukov_alexanderunner@hotmail.com>
2023-06-24 12:04:33 +0300

    second

---
+++ src/components/Header.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+export const Header = () => {
+    return  <header className='header'>
+        <img src={'https://www.reestr35.ru/img/products/big/8509.jpg'}/>
+    </header>
+}
