98c2166228f1b7a6487d18b1f0f59f151a2cc5e3
Aleksandr <strukov_alexanderunner@hotmail.com>
2023-06-24 15:27:35 +0300

    third

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Header.css';
 export const Header = () => {
     return  <header className='header'>
         <img src={'https://www.reestr35.ru/img/products/big/8509.jpg'}/>
