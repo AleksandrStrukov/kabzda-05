0e543e319fc8e983dbfd4ef54af1fdd96b4b54a3
Aleksandr <strukov_alexanderunner@hotmail.com>
2023-07-05 22:48:21 +0300

    12

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,34 @@
 import React from "react";
-
-export const Dialogs =() => {
+import classes from "./Dialogs.module.css"
+export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                Alex
+                </div>
+                <div className={classes.dialog}>
+                    Sara
+                </div>
+                <div className={classes.dialog}>
+                    Tania
+                </div>
+                <div className={classes.dialog}>
+                    Alexey
+                </div>
+                <div className={classes.dialog}>
+                    Alex
+                </div>
+                <div className={classes.dialog}>
+                    Alex
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hey!</div>
+                <div className={classes.message}>How is the weather like today</div>
+                <div className={classes.message}>Whats up?</div>
+                <div className={classes.message}>Here are you!</div>
+            </div>
         </div>
-       )
+    )
 }
