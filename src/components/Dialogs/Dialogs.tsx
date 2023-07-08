83eda55f7e1386c7bf558fcd3c04e84be5b10cb1
Aleksandr <strukov_alexanderunner@hotmail.com>
2023-07-08 20:28:25 +0300

    14

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,34 +2,36 @@ import React from "react";
 import classes from "./Dialogs.module.css"
 import {NavLink} from "react-router-dom";
 
+
+const DialogItem = (props: any) => {
+    let path = '/dialogs/'+ props.id
+    return  <div className={classes.dialog + ' ' + classes.active}>
+        <NavLink to={path}>{props.name}</NavLink>
+    </div>
+}
+
+const MessageItem = (props:any) => {
+    return <div className={classes.message}>{props.message}</div>
+}
+
 export const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
+
             <div className={classes.dialogsItems}>
-                <div className={classes.dialog + ' ' + classes.active}>
-                    <NavLink to='/dialogs/1'>Alex</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/2'> Sara</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/3'>Tania</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/4'> Alexey</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/5'> Grisha</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/6'>Maks</NavLink>
-                </div>
+                <DialogItem name= 'Alex' id='1'/>
+                <DialogItem name= 'Sara' id='2'/>
+                <DialogItem name= 'Tania' id='3'/>
+                <DialogItem name= 'Alexey' id='4'/>
+                <DialogItem name= 'Grisha' id='5'/>
+                <DialogItem name= 'Maks' id='6'/>
+
             </div>
             <div className={classes.messages}>
-                <div className={classes.message}>Hey!</div>
-                <div className={classes.message}>How is the weather like today</div>
-                <div className={classes.message}>Whats up?</div>
-                <div className={classes.message}>Here are you!</div>
+                <MessageItem message='Hey!'/>
+                <MessageItem message='How is the weather like today!'/>
+                <MessageItem message='Whats up!'/>
+                <MessageItem message='Here are you!'/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import  classes from "./MyPosts.module.css"
+import classes from "./MyPosts.module.css"
 import {Post} from "./post/Post";
+
 export const MyPosts = () => {
-    return   <div>
+    return <div>
+        <div>
+           <h3>My posts</h3>
+        </div>
+        <div>
             <div>
-                My posts
-            </div>
-            <div>
-                New post
-            </div>
-            <div className={classes.posts}>
-                <Post message={'Hey!Are you OK?'} like = {12}/>
-                <Post message={"JOhn is come in!"} like = {20}/>
+                <textarea></textarea>
 
             </div>
-        </div>;
+            <button>Add post</button>
+        </div>
+
+        <div className={classes.posts}>
+            <Post message={'Hey!Are you OK?'} like={12}/>
+            <Post message={"JOhn is come in!"} like={20}/>
+
+        </div>
+    </div>;
 }
