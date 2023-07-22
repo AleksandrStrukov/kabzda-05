99a0f7b284a2d69741aa483231398b1aa72a634c
Aleksandr <strukov_alexanderunner@hotmail.com>
2023-07-22 21:50:21 +0300

    30

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,31 +1,46 @@
-import React from "react";
+import React, {createRef, useRef} from "react";
 import classes from "./MyPosts.module.css"
 import {Post} from "./post/Post";
+import {state} from "../../../redux/state";
+
+type myPostsPropsType = {
+    message: string;
+    like: number
+
+}
+
+export const MyPosts = (props: any) => {
+    let newPostElement = useRef<HTMLTextAreaElement>(null);
+    ;
+
+    let postElement = props.posts
+        .map((p: any) => <Post message={p.message} like={p.like}/>)
 
 
-export const MyPosts = (props:any) => {
+    // let newPostElement = createRef();
+    let addPost = () => {
+        if (newPostElement.current) {
+            let text = newPostElement.current.value;
+            alert(text);
+        }
+        };
 
-
-let postElement = props.posts
-    .map((p: { message: string; likesCount: number; }) =>  <Post message={p.message} like={p.likesCount}/>)
-
-
-
-    return <div>
-        <div>
-           <h3>My posts</h3>
-        </div>
-        <div>
+        return <div>
             <div>
-                <textarea></textarea>
+                <h3>My posts</h3>
+            </div>
+            <div>
+                <div>
+
+                    <textarea ref={newPostElement}/>
+
+                </div>
+                <button onClick={addPost}>Add post</button>
+            </div>
+
+            <div className={classes.posts}>
+                {postElement}
 
             </div>
-            <button>Add post</button>
-        </div>
-
-        <div className={classes.posts}>
-            {postElement}
-
-        </div>
-    </div>;
-}
+        </div>;
+    };
